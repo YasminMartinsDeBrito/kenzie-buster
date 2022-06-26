67cf1d8ebba51c1fe16a875bdf40969c860aa6f9
yasmin <yasminmartinsdebrito@gmail.com>
2022-06-26 14:26:57 -0300

    done tw3o

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,29 @@
+// import app from "./app";
+// import { AppDataSource } from "./data-source";
+
+// AppDataSource.initialize()
+//   .then(() => {
+//     console.log("Database connected!");
+//     const port = process.env.PORT ?? 3000;
+
+//     app.listen(port, () => {
+//       console.log(`App running on http://localhost:${port}/`);
+//     });
+//   })
+//   .catch((err) => console.error(err));
+
 import app from "./app";
 import { AppDataSource } from "./data-source";
+import dotenv from "dotenv";
 
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Database connected!");
-    const port = process.env.PORT ?? 3000;
+dotenv.config();
 
-    app.listen(port, () => {
-      console.log(`App running on http://localhost:${port}/`);
-    });
-  })
-  .catch((err) => console.error(err));
+(async () => {
+  await AppDataSource.initialize().catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
+
+  app.listen(process.env.PORT, () =>
+    console.log(`Running at http://localhost:${process.env.PORT}`)
+  );
+})();
