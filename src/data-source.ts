b1632742fe1a2f0777331331369ae0172bd9e394
yasmin <yasminmartinsdebrito@gmail.com>
2022-06-26 19:50:36 -0300

    ok

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,47 +1,47 @@
-// import "reflect-metadata"
-// import { DataSource } from "typeorm"
-// import path from "path"
-// import * as dotenv from 'dotenv'
-// dotenv.config()
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import path from "path"
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-// export const AppDataSource = new DataSource({
-//     type: "postgres",
-//     url: process.env.DATABASE_URL,
-//     logging: false,
-//     ssl:{rejectUnauthorized: false},
-//     entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
-//     migrations: [path.join(__dirname, "./migrations/**/*.{js,ts}")],
-// })
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    logging: false,
+    ssl:{rejectUnauthorized: false},
+    entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
+    migrations: [path.join(__dirname, "./migrations/**/*.{js,ts}")],
+})
 
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import path from "path";
-import dotenv from "dotenv";
+// import "reflect-metadata";
+// import { DataSource } from "typeorm";
+// import path from "path";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-export const AppDataSource =
-  process.env.NODE_ENV === "test"
-    ? new DataSource({
-        type: "postgres",
-        entities: [path.join(__dirname, "/entities/*.ts")],
-        synchronize: true,
-      })
-    : new DataSource({
-        type: "postgres",
-        url: process.env.DATABASE_URL,
-        ssl:
-          process.env.NODE_ENV === "production"
-            ? { rejectUnauthorized: false }
-            : false,
-        synchronize: false,
-        logging: true,
-        entities:
-          process.env.NODE_ENV === "production"
-            ? ["dist/src/entities/*.js"]
-            : ["src/entities/*.ts"],
-        migrations:
-          process.env.NODE_ENV === "production"
-            ? ["dist/src/migrations/*.js"]
-            : ["src/migrations/*.ts"],
-      });
+// export const AppDataSource =
+//   process.env.NODE_ENV === "test"
+//     ? new DataSource({
+//         type: "postgres",
+//         entities: [path.join(__dirname, "/entities/*.ts")],
+//         synchronize: true,
+//       })
+//     : new DataSource({
+//         type: "postgres",
+//         url: process.env.DATABASE_URL,
+//         ssl:
+//           process.env.NODE_ENV === "production"
+//             ? { rejectUnauthorized: false }
+//             : false,
+//         synchronize: false,
+//         logging: true,
+//         entities:
+//           process.env.NODE_ENV === "production"
+//             ? ["dist/src/entities/*.js"]
+//             : ["src/entities/*.ts"],
+//         migrations:
+//           process.env.NODE_ENV === "production"
+//             ? ["dist/src/migrations/*.js"]
+//             : ["src/migrations/*.ts"],
+//       });
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,29 @@
-// import app from "./app";
-// import { AppDataSource } from "./data-source";
-
-// AppDataSource.initialize()
-//   .then(() => {
-//     console.log("Database connected!");
-//     const port = process.env.PORT ?? 3000;
-
-//     app.listen(port, () => {
-//       console.log(`App running on http://localhost:${port}/`);
-//     });
-//   })
-//   .catch((err) => console.error(err));
-
 import app from "./app";
 import { AppDataSource } from "./data-source";
-import dotenv from "dotenv";
 
-dotenv.config();
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connected!");
+    const port = process.env.PORT ?? 3000;
 
-(async () => {
-  await AppDataSource.initialize().catch((err) => {
-    console.error("Error during Data Source initialization", err);
-  });
+    app.listen(port, () => {
+      console.log(`App running on http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => console.error(err));
 
-  app.listen(process.env.PORT, () =>
-    console.log(`Running at http://localhost:${process.env.PORT}`)
-  );
-})();
+// import app from "./app";
+// import { AppDataSource } from "./data-source";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// (async () => {
+//   await AppDataSource.initialize().catch((err) => {
+//     console.error("Error during Data Source initialization", err);
+//   });
+
+//   app.listen(process.env.PORT, () =>
+//     console.log(`Running at http://localhost:${process.env.PORT}`)
+//   );
+// })();
