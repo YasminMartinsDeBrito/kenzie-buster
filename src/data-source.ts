800c35bcbfc0a680cd6f826db11e38ef11f5cd1a
yasmin <yasminmartinsdebrito@gmail.com>
2022-06-26 21:33:05 -0300

    feat:terminei

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,36 +12,3 @@ export const AppDataSource = new DataSource({
     entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
     migrations: [path.join(__dirname, "./migrations/**/*.{js,ts}")],
 })
-
-// import "reflect-metadata";
-// import { DataSource } from "typeorm";
-// import path from "path";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// export const AppDataSource =
-//   process.env.NODE_ENV === "test"
-//     ? new DataSource({
-//         type: "postgres",
-//         entities: [path.join(__dirname, "/entities/*.ts")],
-//         synchronize: true,
-//       })
-//     : new DataSource({
-//         type: "postgres",
-//         url: process.env.DATABASE_URL,
-//         ssl:
-//           process.env.NODE_ENV === "production"
-//             ? { rejectUnauthorized: false }
-//             : false,
-//         synchronize: false,
-//         logging: true,
-//         entities:
-//           process.env.NODE_ENV === "production"
-//             ? ["dist/src/entities/*.js"]
-//             : ["src/entities/*.ts"],
-//         migrations:
-//           process.env.NODE_ENV === "production"
-//             ? ["dist/src/migrations/*.js"]
-//             : ["src/migrations/*.ts"],
-//       });
--- src/server.ts
+++ src/server.ts
@@ -11,19 +11,3 @@ AppDataSource.initialize()
     });
   })
   .catch((err) => console.error(err));
-
-// import app from "./app";
-// import { AppDataSource } from "./data-source";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// (async () => {
-//   await AppDataSource.initialize().catch((err) => {
-//     console.error("Error during Data Source initialization", err);
-//   });
-
-//   app.listen(process.env.PORT, () =>
-//     console.log(`Running at http://localhost:${process.env.PORT}`)
-//   );
-// })();
